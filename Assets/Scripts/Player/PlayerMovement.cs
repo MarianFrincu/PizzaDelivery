21024beb22bede4579a8531c32eb87ecb66f8afc
Marian <marianxd2345@gmail.com>
2024-12-29 03:02:23 +0200

    Added collisions and movement for the motorcycle

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,33 +1,122 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float _MovementSpeed;
-    [SerializeField] private float _RotationSpeed;
+    [SerializeField] private float _MaxSpeed = 20f;
+    [SerializeField] private float _ReverseSpeed = 5f;
+    [SerializeField] private float _Acceleration = 5f;
+    [SerializeField] private float _Deceleration = 8f;
+    [SerializeField] private float _RotationSpeed = 100f;
+    [SerializeField] private Transform _SteeringHelper;
+    [SerializeField] private float _MinYPosition = 0f;
 
-    void Start()
+    private Rigidbody _rb;
+    private float _currentSpeed = 0f;
+    private bool _isReversing = false;
+
+    private void Start()
     {
-
+        _rb = GetComponent<Rigidbody>();
+        _rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+        _rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
     }
 
-    void Update()
+    private void FixedUpdate()
     {
+        Move();
+        RotateHandlebars();
+        PreventOutOfMap();
+    }
+
+    private void Move()
+    {
+        float forwardInput = 0f;
+
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            transform.position += _MovementSpeed * Time.deltaTime * transform.forward;
+            forwardInput = 1f;
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        else if (Input.GetKey(KeyCode.DownArrow))
         {
-            transform.position += _MovementSpeed * Time.deltaTime * -transform.forward;
+            forwardInput = -1f;
         }
+
+        _isReversing = _currentSpeed < 0;
+
+        if (forwardInput > 0)
+        {
+            _currentSpeed = Mathf.MoveTowards(_currentSpeed, forwardInput * _MaxSpeed, _Acceleration * Time.fixedDeltaTime);
+        }
+        else if (forwardInput < 0)
+        {
+            _currentSpeed = Mathf.MoveTowards(_currentSpeed, forwardInput * _ReverseSpeed, _Acceleration * Time.fixedDeltaTime);
+        }
+        else
+        {
+            _currentSpeed = Mathf.MoveTowards(_currentSpeed, 0, _Deceleration * Time.fixedDeltaTime);
+        }
+
+        Vector3 forwardMovement = transform.forward * _currentSpeed * Time.fixedDeltaTime;
+
+        _rb.MovePosition(_rb.position + forwardMovement);
+
+        // Rotim motocicleta
         if (Input.GetKey(KeyCode.LeftArrow))
+            RotateVehicle(-1);
+        else if (Input.GetKey(KeyCode.RightArrow))
+            RotateVehicle(1);
+    }
+
+    private void RotateVehicle(float direction)
+    {
+        float rotationFactor = Mathf.Clamp01(Mathf.Abs(_currentSpeed) / _MaxSpeed);
+        float rotationDirection = _isReversing ? -1 : 1;
+        Quaternion deltaRotation = Quaternion.Euler(0, rotationDirection * direction * _RotationSpeed * rotationFactor * Time.fixedDeltaTime, 0);
+        _rb.MoveRotation(_rb.rotation * deltaRotation);
+    }
+
+    private void RotateHandlebars()
+    {
+        float targetZ = 0;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            targetZ = -35;
+        else if (Input.GetKey(KeyCode.RightArrow))
+            targetZ = 35;
+
+        Vector3 targetEulerAngles = new Vector3(
+            _SteeringHelper.localEulerAngles.x,
+            _SteeringHelper.localEulerAngles.y,
+            Mathf.Lerp(NormalizeAngle(_SteeringHelper.localEulerAngles.z), targetZ, Time.fixedDeltaTime * 5f)
+        );
+
+        _SteeringHelper.localEulerAngles = targetEulerAngles;
+    }
+
+    private void PreventOutOfMap()
+    {
+        if (transform.position.y < _MinYPosition)
         {
-            transform.eulerAngles += _RotationSpeed * Time.deltaTime * Vector3.down;
+            Vector3 newPosition = _rb.position;
+            newPosition.y = _MinYPosition;
+            _rb.MovePosition(newPosition);
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+    }
+
+    private float NormalizeAngle(float angle)
+    {
+        if (angle > 180) angle -= 360;
+        if (angle < -180) angle += 360;
+        return angle;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
         {
-            transform.eulerAngles += _RotationSpeed * Time.deltaTime * Vector3.up;
+            _currentSpeed = 0;
+            _rb.linearVelocity = Vector3.zero;
+            Debug.Log("Hit: " + collision.gameObject.name);
         }
     }
 }
