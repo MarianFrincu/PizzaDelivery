41c5e45a1ed5f883e2b26d14e11995ed4b2f9e4e
Lacovei Cristina-Daniela <lacoveicristina03@gmail.com>
2025-01-06 19:40:29 +0200

    implemented high score button

---
+++ Assets/Scripts/HighScoreDisplay.cs
@@ -0,0 +1,28 @@
+﻿using TMPro; 
+using UnityEngine;
+
+public class HighScoreDisplay : MonoBehaviour
+{
+    [SerializeField] private TMP_Text easyScoreText;
+    [SerializeField] private TMP_Text mediumScoreText;
+    [SerializeField] private TMP_Text hardScoreText;
+
+    public void ShowHighScores()
+    {
+        HighScoreManager highScoreManager = FindObjectOfType<HighScoreManager>();
+
+        if (highScoreManager != null)
+        {
+            Debug.Log($"Easy: {highScoreManager.GetHighScore(DifficultyManager.Difficulty.Easy)}, Medium: {highScoreManager.GetHighScore(DifficultyManager.Difficulty.Medium)}, Hard: {highScoreManager.GetHighScore(DifficultyManager.Difficulty.Hard)}");
+
+            easyScoreText.text = $"Easy: {highScoreManager.GetHighScore(DifficultyManager.Difficulty.Easy)}";
+            mediumScoreText.text = $"Medium: {highScoreManager.GetHighScore(DifficultyManager.Difficulty.Medium)}";
+            hardScoreText.text = $"Hard: {highScoreManager.GetHighScore(DifficultyManager.Difficulty.Hard)}";
+        }
+        else
+        {
+            Debug.LogError("HighScoreManager nu a fost găsit!");
+        }
+    }
+
+}
